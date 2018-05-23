5f1adacb9ab2c671ea6aedf60136b7d1c6aedf88
Nicolas Lound <nlound@gmail.com>
2018-05-23 03:05:38 -0300

    Add logic to list directories

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,37 @@
 
 const gulp = require('gulp');
+const fs = require('fs');
+const path = require('path');
+const baseDir = "dist/"
+const targetFile = "file.png";
 
 // default task
 gulp.task('default', () =>  {
-  console.log ('Running default task...');
+  let dirs = getDirs(baseDir,targetFile);
+  console.log (dirs)
 });
+
+
+/**
+ * Returns an array of directories containing certain file
+ * @param {string} dir
+ *  Root directory for the seach
+ * @param {string} flagFile
+ *  File to found
+ */
+function getDirs (dir, flagFile, dirList = []) {
+  fs.readdirSync(dir).forEach(file => {
+    const dirFile = path.join(dir, file);
+      try {
+        dirList = getDirs(dirFile, flagFile, dirList);
+      }
+      catch (err) {
+        if (err.code === 'ENOTDIR') {
+          file === flagFile ? dirList.push( dir.replace(baseDir,'') ):'';
+        }else{
+          throw err;
+        }
+      }
+  });
+  return dirList;
+}
